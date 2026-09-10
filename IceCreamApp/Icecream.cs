c0b04057b605c7ba6ea02e8cf380116128e8e6df
ibbe479 <ibrahim.halawani04@gmail.com>
2026-09-10 20:18:48 +0200

    lec1

---
+++ IceCreamApp/Icecream.cs
@@ -0,0 +1,19 @@
+﻿namespace IceCreamApp;
+internal class Icecream
+{
+    private string flavor = "vanila" ;//cherry
+    private int numOfScoops = 2;
+    private double pricePerScoop = 35.99;
+
+    //operations ---> methods 
+
+    public void calculatePrice()
+    {
+        double totalPrice = numOfScoops * pricePerScoop;
+        Console.WriteLine("The total price is " + totalPrice);
+    }
+
+    // read input
+    //calculation 
+    // show output
+}
---
+++ IceCreamApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace IceCreamApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Icecream dessert = new Icecream();
+        Icecream fika = new Icecream();
+
+        dessert.calculatePrice();
+        
+    }
+}
